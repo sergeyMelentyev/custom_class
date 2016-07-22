ebf64797f192ac075e0d0d746140e46d5037c3b1
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-07-22 19:33:11 +0400

    Create custom_class.h

---
+++ custom_class.h
@@ -0,0 +1,14 @@
+//
+// Created by Melentyev on 22.07.16.
+//
+
+#ifndef CPP_CLASS_CUSTOM_CLASS_H
+#define CPP_CLASS_CUSTOM_CLASS_H
+
+
+class custom_class {
+
+};
+
+
+#endif //CPP_CLASS_CUSTOM_CLASS_H
